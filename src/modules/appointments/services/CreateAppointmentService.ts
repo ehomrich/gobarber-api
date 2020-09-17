92a145f2c2eae55cf35f1bdefcd46a23094f5697
Émerson P. Homrich <12125514+ehomrich@users.noreply.github.com>
2020-09-16 22:59:02 -0300

    Add provider notifications

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,11 @@
 import { injectable, inject } from 'tsyringe';
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
 
-import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
 interface IRequestBody {
   provider_id: string;
@@ -17,6 +18,8 @@ class CreateAppointmentService {
   constructor(
     @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
+    @inject('NotificationsRepository')
+    private notificationsRepository: INotificationsRepository,
   ) {}
 
   public async execute({
@@ -52,6 +55,13 @@ class CreateAppointmentService {
       provider_id,
       user_id,
       date: appointmentDate,
+    });
+
+    const formattedDate = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'");
+
+    await this.notificationsRepository.create({
+      recipient_id: provider_id,
+      content: `Novo agendamento para ${formattedDate}`,
     });
 
     return appointment;
---
+++ src/modules/notifications/infra/typeorm/schemas/Notification.ts
@@ -0,0 +1,34 @@
+import {
+  Entity,
+  ObjectID,
+  ObjectIdColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('notifications')
+class Notification {
+  @ObjectIdColumn()
+  id: ObjectID;
+
+  @Column()
+  content: string;
+
+  @Column('uuid')
+  recipient_id: string;
+
+  @Column({ default: false })
+  read: boolean;
+
+  @Column('text')
+  date: Date;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Notification;
